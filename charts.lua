a8b03f8d9528b9de25adc26b9785eba05cdb1a5e
Christopher Moore <christopher.e.moore@gmail.com>
2025-06-04 12:54:18 +0800

    moved the circle calcs to the shader, fixed some rotation stuff about them

---
+++ charts.lua
@@ -0,0 +1,27 @@
+-- [=======[ TODO cache calcs somehow
+local allCharts = require 'geographic-charts'
+local chartNames = table{	-- which charts we want to allow ...
+	'Equirectangular',
+	'Azimuthal equidistant',
+	'Mollweide',
+	'WGS84',
+}
+local charts = table()
+for i,name in ipairs(chartNames) do
+	local chart = allCharts![name]
+	charts[i] = chart
+	charts[name] = chart
+end
+for _,name in ipairs(chartNames) do
+	charts[name]?:build()
+end
+
+local chartCNames = charts:mapi(|chart| chart:getCName())
+local chartCode = require 'geographic-charts.code'(charts)
+--]=======]
+
+return {
+	charts = charts,
+	chartCode = chartCode, 
+	chartCNames = chartCNames,
+}
