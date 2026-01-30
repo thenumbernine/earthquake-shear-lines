2ca01923c4cf9c84dcd05488c45052ba8952b90e
Christopher Moore <christopher.e.moore@gmail.com>
2026-01-30 14:24:01 +0700

    segfaults were happening when i used both tex2d and tex3d ... until i switched away from langfix to vanilla luajit ... hmm ...

---
+++ viz-hist-splat-volume.lua
@@ -0,0 +1,386 @@
+#!/usr/bin/env luajit
+--[[
+splat all quakes onto a volumetric binning of lat x lon x time
+--]]
+local ffi = require 'ffi'
+require 'ext'
+require 'vec-ffi'
+require 'gl.env'()
+local vector = require 'ffi.cpp.vector-lua'
+local ig = require 'imgui'
+
+local volSplatCachePath = path'volumesplatcache.txt'
+
+local cache
+if volSplatCachePath:exists() then
+	cache = assert(volSplatCachePath:read())
+else
+	local t = timer.time()
+print('ostime', t)
+	local tt = timer.gmtime(t)
+print('gmtime', tolua(tt))
+	local Julian = require 'solarsystem.julian'
+	local julianDay = Julian.fromCalendar(tt)
+print('julianday', julianDay)	
+timer('building cache', function()
+	local lonRes = 360
+	local latRes = 180
+	local timeRes = 360
+
+	local numDays = 360
+
+	local dayInSec = require 'basis'.dayInSec
+	local getEarthquakes = require 'earthquakes'.getEarthquakes
+	local earthquakes = getEarthquakes(numDays)
+
+	local densitySize = vec3i(lonRes, latRes, timeRes)
+	local densityData = ffi.new('float[?]', densitySize:volume())
+_G.densityData = densityData 
+	for i=#earthquakes,1,-1 do
+		local eq = earthquakes[i]
+
+		local lonBin = math.floor((((eq.longitude + 180) / 360) % 1) * lonRes)
+		local latBin = math.floor((((eq.latitude + 90) / 180) % 1) * latRes)
+		local timeBin = math.floor((julianDay - eq.julianDay) / numDays * timeRes)
+
+		assert.le(0, lonBin)
+		assert.lt(lonBin, lonRes)
+		assert.le(0, latBin)
+		assert.lt(latBin, latRes)
+		assert.le(0, timeBin)
+		if timeBin >= timeRes then break end
+
+		local energy = 10 ^ (1.5 * eq.mag + 4.4)	-- in joules
+
+		local densityIndex = lonBin + lonRes * (latBin + latRes * timeBin)
+		densityData[densityIndex] = densityData[densityIndex] + energy
+	end
+	cache = ffi.string(densitySize.s, ffi.sizeof'vec3i_t')
+		..ffi.string(densityData, ffi.sizeof(densityData))
+	assert.len(cache, ffi.sizeof'vec3i_t' + ffi.sizeof'float' * densitySize:volume())
+	volSplatCachePath:write(cache)
+end)
+end
+assert.type(cache, 'string')
+assert.ge(#cache, ffi.sizeof'vec3i_t')
+local densitySize = ffi.cast('vec3i_t*', cache)[0]
+print('densitySize', densitySize)
+assert.len(cache, ffi.sizeof'vec3i_t' + ffi.sizeof'float' * densitySize:volume())
+_G.densityData = ffi.new('float[?]', densitySize:volume())
+ffi.copy(densityData, ffi.cast('char*', cache) + ffi.sizeof'vec3i_t', ffi.sizeof'float' * densitySize:volume())
+
+
+local logDensityEpsilon = 1e-5
+
+_G.densityRange = vec2f(math.huge, -math.huge)
+for i=0,densitySize:volume()-1 do
+	local v = densityData[i]
+	densityRange.x = math.min(densityRange.x, v)
+	densityRange.y = math.max(densityRange.y, v)
+end
+print('density range', densityRange)
+_G.logDensityRange = densityRange:map(function(x) return math.log(x + logDensityEpsilon) end)
+print('log density range', logDensityRange)
+
+_G.logDensityAlphaRange = vec3f(0, 1, 1)	-- .x = min, .y = max, .z = clamped scale
+
+_G.volumeRange = box3f(
+	vec3f(-1, -.5, -.1),
+	vec3f(1, .5, .1)
+)
+
+
+local App = require 'imgui.appwithorbit'()
+
+function App:initGL()
+	App.super.initGL(self)
+
+
+-- [=[
+	self.mapTex = GLTex2D{
+		filename = 'earth-color.png',
+		magFilter = gl.GL_LINEAR,
+		minFilter = gl.GL_LINEAR_MIPMAP_LINEAR,
+		generateMipmap = true,
+		wrap = {
+			s = gl.GL_REPEAT,
+			t = gl.GL_CLAMP_TO_BORDER,
+		},
+	}:unbind()
+
+	self.mapObj = GLSceneObject{
+		program = {
+			version = 'latest',
+			precision = 'best',
+			vertexCode = [[
+layout(location=0) in vec2 vertex;
+layout(location=0) out vec2 tc;
+uniform mat4 mvProjMat;
+uniform vec3 volumeMin, volumeMax;
+void main() {
+	tc = vec2(vertex.x, 1. - vertex.y);
+	gl_Position = mvProjMat * vec4(
+		mix(volumeMin, volumeMax, vec3(vertex, 0.)),
+		1.
+	);
+}
+]],
+			fragmentCode = [[
+layout(location=0) in vec2 tc;
+layout(location=0) out vec4 fragColor;
+uniform sampler2D tex;
+void main() {
+	fragColor = texture(tex, tc);
+}
+]],
+			uniforms = {
+				tex = 0,
+			},
+		},
+		vertexes = {
+			dim = 2,
+			data = {0,0, 1,0, 0,1, 1,1},
+		},
+		geometry = {
+			mode = gl.GL_TRIANGLE_STRIP,
+		},
+		texs = {
+			self.mapTex,
+		},
+	}
+--]=]
+
+
+-- [=[
+	_G.lineVtxCPU = vector'vec3f_t'
+	local cornerbits = function(i)
+		return bit.band(i,1),
+			bit.band(bit.rshift(i,1),1),
+			bit.band(bit.rshift(i,2),1)
+	end
+	for i=0,7 do
+		for j=0,2 do
+			local k = bit.bxor(i, bit.lshift(1, j))
+			if k > i then
+				lineVtxCPU:emplace_back()[0]:set(cornerbits(i))
+				lineVtxCPU:emplace_back()[0]:set(cornerbits(k))
+			end
+		end
+	end
+	self.lineObj = GLSceneObject{
+		program = {
+			version = 'latest',
+			precision = 'best',
+			vertexCode = [[
+layout(location=0) in vec3 vertex;
+uniform mat4 mvProjMat;
+uniform vec3 volumeMin, volumeMax;
+void main() {
+	gl_Position = mvProjMat * vec4(
+		mix(volumeMin, volumeMax, vertex),
+		1.
+	);
+}
+]],
+			fragmentCode = [[
+layout(location=0) out vec4 fragColor;
+
+void main() {
+	fragColor = vec4(.5, .5, .5, 1.);
+}
+]],
+		},
+		vertexes = {
+			dim = 3,
+			data = lineVtxCPU.v,
+			size = lineVtxCPU:getNumBytes(),
+		},
+		geometry = {
+			mode = gl.GL_LINES,
+			count = #lineVtxCPU,
+		},
+	}
+--]=]
+
+
+	self.hsvTex = GLHSVTex2D(256, nil, true)
+		:unbind()
+
+	self.vtxGPUsPerSide = range(0,5):mapi(function(side)
+		local axis = side % 3
+		local pm = side >= 3
+		
+		local k = axis
+		local k2 = (axis + 1) % 3
+		local k3 = (axis + 2) % 3
+		local function set(v, x,y,z)
+			v.s[k2] = x
+			v.s[k3] = y
+			v.s[k] = z
+		end
+
+		-- regenerate voxel rendering geometry data as per our slice resolution
+		-- TODO one per 6 sides
+		local sliceRes = 360
+		local vtxGPU = GLArrayBuffer{
+			dim = 3,
+			useVec = true,
+		}
+		local vtxCPU = vtxGPU:beginUpdate()
+		local imin, imax, istep
+		if pm then
+			imin, imax, istep = 0, sliceRes-1, 1
+		else
+			imin, imax, istep = sliceRes-1, 0, -1
+		end
+		for i=imin,imax,istep do
+			local f = (i + .5) / sliceRes
+			set(vtxCPU:emplace_back(), 0, 0, f)
+			set(vtxCPU:emplace_back(), 1, 0, f)
+			set(vtxCPU:emplace_back(), 0, 1, f)
+			set(vtxCPU:emplace_back(), 1, 0, f)
+			set(vtxCPU:emplace_back(), 0, 1, f)
+			set(vtxCPU:emplace_back(), 1, 1, f)
+		end
+		vtxGPU:endUpdate()
+		vtxGPU:unbind()
+		return vtxGPU
+	end)
+--]=]
+
+-- [=[
+	self.volumeTex = GLTex3D{
+		width = densitySize.x,
+		height = densitySize.y,
+		depth = densitySize.z,
+		internalFormat = gl.GL_R32F,
+		data = densityData,
+		magFilter = gl.GL_LINEAR,
+		minFilter = gl.GL_LINEAR_MIPMAP_LINEAR,
+		generateMipmap = true,
+		wrap = {
+			s = gl.GL_CLAMP_TO_EDGE,
+			t = gl.GL_CLAMP_TO_EDGE,
+		},
+	}:unbind()
+
+--[[ how to swap after-the-fact ?
+	self.volumeObj.vertexes = self.vtxGPUsPerSide[1]
+	self.volumeObj.attrs.vertex.buffer = self.volumeObj.vertexes
+	self.volumeObj.geometry.count = #self.volumeObj.vertexes.vec
+--]]
+
+	local volumeVtxGPU = self.vtxGPUsPerSide[3]
+	self.volumeObj = GLSceneObject{
+		program = {
+			version = 'latest',
+			precision = 'best',
+			vertexCode = [[
+layout(location=0) in vec3 vertex;
+layout(location=0) out vec3 tc;
+
+uniform mat4 mvProjMat;
+uniform vec3 volumeMin, volumeMax;
+
+void main() {
+	tc = vertex;
+	gl_Position = mvProjMat * vec4(
+		mix(volumeMin, volumeMax, vertex),
+		1.
+	);
+}
+]],
+			fragmentCode = [[
+layout(location=0) in vec3 tc;
+layout(location=0) out vec4 fragColor;
+
+uniform sampler3D volumeTex;
+uniform sampler2D hsvTex;
+uniform vec2 logDensityRange;
+uniform vec3 logDensityAlphaRange;
+
+void main() {
+	float density = texture(volumeTex, tc).r;
+	float logDensity = log(density + ]]..glnumber(logDensityEpsilon)..[[);
+	float gradLookup = (logDensity - logDensityRange.x) / (logDensityRange.y - logDensityRange.x);
+	fragColor = texture(hsvTex, vec2(gradLookup, .5));
+	fragColor.a = logDensityAlphaRange.z * clamp(
+		(logDensity - logDensityAlphaRange.x) / (logDensityAlphaRange.y - logDensityAlphaRange.x),
+		0.,
+		1.
+	);
+}
+]],
+			uniforms = {
+				volumeTex = 0,
+				hsvTex = 1,
+			},
+		},
+		geometry = {
+			mode = gl.GL_TRIANGLES,
+			count = #volumeVtxGPU.vec,
+		},
+		vertexes = volumeVtxGPU,
+		texs = {
+			self.volumeTex,
+			self.hsvTex,
+		},
+	}
+--]=]
+
+
+	gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+end
+
+function App:update()
+	gl.glClear(bit.bor(gl.GL_COLOR_BUFFER_BIT, gl.GL_DEPTH_BUFFER_BIT))
+
+	if self.mapObj then
+		self.mapObj.uniforms.mvProjMat = self.view.mvProjMat.ptr
+		self.mapObj.uniforms.volumeMin = volumeRange.min.s
+		self.mapObj.uniforms.volumeMax = volumeRange.max.s
+		self.mapObj:draw()
+	end
+
+	if self.lineObj then
+		self.lineObj.uniforms.mvProjMat = self.view.mvProjMat.ptr
+		self.lineObj.uniforms.volumeMin = volumeRange.min.s
+		self.lineObj.uniforms.volumeMax = volumeRange.max.s
+		self.lineObj:draw()
+	end
+
+	if self.volumeObj then
+		gl.glDisable(gl.GL_DEPTH_TEST)
+		gl.glEnable(gl.GL_BLEND)
+		self.volumeObj.uniforms.mvProjMat = self.view.mvProjMat.ptr
+		self.volumeObj.uniforms.logDensityRange = logDensityRange.s
+		self.volumeObj.uniforms.logDensityAlphaRange = logDensityAlphaRange.s
+		self.volumeObj.uniforms.volumeMin = volumeRange.min.s
+		self.volumeObj.uniforms.volumeMax = volumeRange.max.s
+		self.volumeObj:draw()
+		gl.glDisable(gl.GL_BLEND)
+		gl.glEnable(gl.GL_DEPTH_TEST)
+	end
+
+	App.super.update(self)
+end
+
+function App:updateGUI()
+	ig.igInputFloat('logDensityMin', logDensityRange.s + 0)
+	ig.igInputFloat('logDensityMax', logDensityRange.s + 1)
+
+	-- logDensity is normalized to this range [.x,.y], and then clamped to (0,1), and then scaled by .z
+	ig.igInputFloat('logDensityAlphaMin', logDensityAlphaRange.s + 0)
+	ig.igInputFloat('logDensityAlphaMax', logDensityAlphaRange.s + 1)
+	ig.igInputFloat('logDensityAlphaClampScale', logDensityAlphaRange.s + 2)
+	
+	ig.igInputFloat('volumeMinX', volumeRange.min.s + 0)
+	ig.igInputFloat('volumeMinY', volumeRange.min.s + 1)
+	ig.igInputFloat('volumeMinZ', volumeRange.min.s + 2)
+	
+	ig.igInputFloat('volumeMaxX', volumeRange.max.s + 0)
+	ig.igInputFloat('volumeMaxY', volumeRange.max.s + 1)
+	ig.igInputFloat('volumeMaxZ', volumeRange.max.s + 2)
+end
+
+return App():run()
